ddfe8d78778170614b62c79df10efa879743bddd
rushda-101 <rushda.afaf@gmail.com>
2026-03-02 17:03:14 +0000

    Added UI display for coin collection

--- block-bound/game_manager.gd
+++ block-bound/game_manager.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 var score = 0
+var score_label
 
 func _ready():
-	# Find all coins in the scene
+	# Get reference to the label
+	score_label = get_node("../UI/ScoreLabel")
+
+	# Connect all coins
 	for coin in get_tree().get_nodes_in_group("Coins"):
 		coin.connect("collected", Callable(self, "_on_coin_collected"))
 
 func _on_coin_collected():
 	score += 1
-	print("Score: ", score)
+	score_label.text = "Score: " + str(score)
